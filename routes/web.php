b1e0d151c0b930b5983e98bf98a1d324fd0c39b3
Abdulai Wunam <wunamabdulai@gmail.com>
2024-11-21 21:00:34 +0000

    Update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,11 +14,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'AuthLogin']);
+Route::get('logout', [AuthController::class, 'logout']);
+Route::get('forgot-password', [AuthController::class, 'forgotpassword']);
+Route::post('forgot-password', [AuthController::class, 'PostForgotPassword']);
+Route::get('reset/{token}', [AuthController::class, 'reset']);
+Route::post('reset/{token}', [AuthController::class, 'PostReset']);
+
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 
 Route::get('admin/dashboard', function () {
     return view('admin.dashboard');
+});
+
+Route::get('admin/admin/list', function () {
+    return view('admin.admin.list');
+});
+
+Route::group(['middleware' => 'admin'], function(){
+
+    Route::get('admin/dashboard', [DashboardController::class, 'dashboard']);
+
+});
+
+Route::group(['middleware' => 'officer'], function(){
+
+    Route::get('officer/dashboard', [DashboardController::class, 'dashboard']);
+
 });
